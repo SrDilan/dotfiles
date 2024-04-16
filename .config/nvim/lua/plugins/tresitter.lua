7c60d82303d266ecedeae7d426d230e4ae84826b
Dil4n <dilansamper11@gmail.com>
2024-04-16 07:18:02 -0500

    add new config nvim

---
+++ .config/nvim/lua/plugins/tresitter.lua
@@ -0,0 +1,41 @@
+return {
+  {
+
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    highlight = { enable = true },
+    indent = { enable = true },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+}
